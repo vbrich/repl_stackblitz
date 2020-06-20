57f40e30c13dc9df52c03acea47cde9a1aeea749
vbrich <vb_rich@yahoo.com>
2020-06-20 16:14:02 -0700

    Working hardcoded service call, just before adding jsoneditor for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostService } from './post-service';
+import { CunexusComponent } from './cunexus/cunexus.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CunexusComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ PostService ]
 })
